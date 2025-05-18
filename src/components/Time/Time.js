955244e9da199ab5014cff4f57bbb2b0944cb45c
Eduardo Thomé <ethome742@gmail.com>
2025-05-18 01:01:29 -0300

    Tudo o que foi feito até o final do módulo 04

---
+++ src/components/Time/Time.js
@@ -0,0 +1,23 @@
+import Colaborador from '../Colaborador/Colaborador';
+import './Time.css'
+
+const Time = (props) => {
+
+    const css = { backgroundColor: props.corSecundaria }
+
+    // Para realizar renderização condicional podemos utilizar o operador ternário como foi feito abaixo
+    // ou
+    // podemos utilizar o operador '&&' no lugar do '?' e retirar o ':' do final
+    // desse modo se a condição for true já retorna o que vier a seguir
+    return ( 
+        (props.colaboradores.length > 0) ? <section className='time' style = {css}> 
+            <h3 style = { { borderColor: props.corPrimaria } }>{props.nome}</h3>
+            <div className='colaboradores'>
+                {props.colaboradores.map( colaborador => <Colaborador key = {colaborador.nome} nome = {colaborador.nome} cargo = {colaborador.cargo} imagem = {colaborador.imagem}/>)}
+            </div>
+        </section>
+        : ''
+    )
+}
+
+export default Time;
