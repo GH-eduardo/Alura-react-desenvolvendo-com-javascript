b59a1da461fe91de86caf84623ce244a55c55742
Eduardo Thomé <ethome742@gmail.com>
2025-05-19 13:36:46 -0300

    ajustes finais

--- src/components/ListaSuspensa/ListaSuspensa.js
+++ src/components/ListaSuspensa/ListaSuspensa.js
@@ -5,7 +5,8 @@ const ListaSuspensa = (props) => {
     return (
         <div className='lista-suspensa'>
             <label>{props.label}</label>
-            <select onChange = {event => props.aoAlterado(event.target.value)} required = {props.obrigatorio} value = {props.value}>
+            <select onChange = {event => props.aoAlterado(event.target.value)} required = {props.obrigatorio} value = {props.valor}>
+                <option value = ''></option>
                 {props.itens.map(item => <option key={item}>{item}</option>)}
             </select>
         </div>
